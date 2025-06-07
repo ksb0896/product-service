00934d3373028aa73c16591597cf43e5eae06d5e
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-06-07 12:44:58 +0530

    thread --added

--- src/main/java/com/ksb/micro/product/controller/ProductController.java
+++ src/main/java/com/ksb/micro/product/controller/ProductController.java
@@ -26,6 +26,11 @@ public class ProductController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts(){
+/*        try{ //test purpose-dont use in prod.
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }*/
         return productService.getAllProducts();
     }
 }
